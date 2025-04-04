95fd83d0cbf7776c4134709c9cdaf7da1a5b6867
Gift Wamukota <12593712+gwamukota@users.noreply.github.com>
2025-04-04 19:39:24 +0300

    Portfolio Version 1.0

---
+++ index.js
@@ -0,0 +1,22 @@
+function submit() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "service_rjtbn9q";
+  const templateID = "template_03yyrzq";
+
+    emailjs.send(serviceID, templateID, params)
+    .then(res=>{
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Your message sent successfully!!")
+
+    })
+    .catch(err=>console.log(err));
+
+}
